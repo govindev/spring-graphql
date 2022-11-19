9e66e64f37fb02aaf2b32481846b969dd06c709d
Vurukuti Govinda Raju <vgovindev@gmail.com>
2022-11-19 21:35:18 +0530

    error handling

---
+++ src/main/java/com/govind/hplus/controller/HPlusExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.govind.hplus.controller;
+
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+import graphql.schema.DataFetchingEnvironment;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.graphql.execution.ErrorType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HPlusExceptionHandler extends DataFetcherExceptionResolverAdapter {
+    @Override
+    protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+        ErrorType type = null;
+        if (ex instanceof DataIntegrityViolationException) {
+            type = ErrorType.BAD_REQUEST;
+        } else {
+            type = ErrorType.INTERNAL_ERROR;
+        }
+        return GraphqlErrorBuilder.newError(env)
+                .errorType(type)
+                .message("Received Message: " + ex.getMessage())
+                .build();
+    }
+}
